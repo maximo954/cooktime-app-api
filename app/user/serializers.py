4b954d7909479148c880bc04f5b4c22f2b294825
alain <alaing.dev@gmail.com>
2020-01-07 18:57:04 -0500

    Added manage user endpoint

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -16,14 +16,28 @@ class UserSerializer(serializers.ModelSerializer):
         """Create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update a user, setting the password correctly and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
-    """Serializer for users authentication object"""
+    """Serializer for the user authentication object"""
     email = serializers.CharField()
-    password = serializers.CharField(style={'input_type': 'password'},
-                                     trim_whitespace=False)
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False
+    )
 
     def validate(self, attrs):
+        """Validate and authenticate the user"""
         email = attrs.get('email')
         password = attrs.get('password')
 
@@ -33,7 +47,7 @@ class AuthTokenSerializer(serializers.Serializer):
             password=password
         )
         if not user:
-            msg = _('Unable to authenticate with credentials provided.')
+            msg = _('Unable to authenticate with provided credentials')
             raise serializers.ValidationError(msg, code='authentication')
 
         attrs['user'] = user
